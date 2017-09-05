6565c35952d3789cacf8a3e4ec83a277cb2ce9b8
Maxime COLIN <maxime.colin@elao.com>
2017-09-05 11:06:58 +0200

    PS-2017.2.1 <maximecolin@macbook-pro-de-maxime.local Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, Default copy.xml, git.xml, other.xml, PHP Class Doc Comment.php, Zen CSS.xml, Default.xml, AngularJS .xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, Symfony.xml, filetypes.xml, php.xml, code.style.schemes.xml, colors.scheme.xml, Angular2.xml, Zen HTML.xml, PHP Getter Method.php, Default.xml, PHP.xml, vue_for_idea.xml, PHP File Header.php, Zen XSL.xml, laf.xml, Default _1_.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, PHP Setter Method.php, databaseSettings.xml, Jinja2.xml, keymap.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/includes/PHP Class Doc Comment.php
@@ -0,0 +1,7 @@
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,9 @@
+
+/*
+ * This file is part of the ${PROJECT_NAME} project.
+ *
+ * Copyright (C) ${YEAR} ${PROJECT_NAME}
+ *
+ * @author Elao <contact@elao.com>
+ */
+ 
